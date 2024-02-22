51eb6e7c9f252fe5fb0825549e27552c7c94382b
bgallois <benjamin@gallois.cc>
2024-02-22 23:07:35 +0100

    [feat] add function expr decorator

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,30 @@
-use weight_decorator_derive::weight;
 use frame_support::pallet_prelude::Weight;
+use weight_decorator_derive::weight;
+use weight_decorator_derive::weight_function;
 
 #[test]
-fn test() {
+fn test_exp() {
     let test = weighted_do_something(2);
-    println!("{}", test);
+    assert_eq!(test, Weight::zero());
+}
+
+#[test]
+fn test_func() {
+    let test = weighted_do_something_else(2, 4);
+    assert_eq!(test, Weight::from_parts(10, 10));
 }
 
 #[weight(Weight::zero())]
 fn do_something(i: u32) -> u32 {
     i * i
+}
+
+#[allow(dead_code)]
+fn weight_for_do_something_else(_i: u32, _j: u32) -> Weight {
+    Weight::from_parts(10, 10)
+}
+
+#[weight_function(weight_for_do_something_else)]
+fn do_something_else(i: u32, j: u32) -> u32 {
+    i * j
 }
--- weight-decorator-derive/src/lib.rs
+++ weight-decorator-derive/src/lib.rs
@@ -1,11 +1,11 @@
-use proc_macro::Span;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn;
+use syn::Expr;
+use syn::FnArg;
 use syn::Ident;
 use syn::ItemFn;
-use syn::{Expr, Result};
-use frame_support::pallet_prelude::Weight;
+use syn::Pat;
 
 #[proc_macro_attribute]
 pub fn weight(attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -25,6 +25,39 @@ pub fn weight(attr: TokenStream, item: TokenStream) -> TokenStream {
     fn #varname(#inputs) -> Weight{
         let _ = #body;
         #ast_attr
+    }
+    };
+    gen.into()
+}
+
+#[proc_macro_attribute]
+pub fn weight_function(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Parse function
+    let ast: ItemFn = syn::parse(item).expect("Failed to parse input as a function item!");
+    let name = &ast.sig.ident;
+    let fname = format!("weighted_{}", name);
+    let varname = syn::Ident::new(&fname, name.span());
+    let body = &ast.block;
+    let inputs = &ast.sig.inputs;
+
+    // Parse weight annotation
+    let ast_attr: Ident =
+        syn::parse(attr).expect("Failed to parse weight annotation as a function to evaluate!");
+
+    let mut test: syn::punctuated::Punctuated<syn::Ident, syn::token::Comma> =
+        syn::punctuated::Punctuated::new();
+    for i in inputs {
+        if let FnArg::Typed(a) = i {
+            if let Pat::Ident(c) = *a.pat.clone() {
+                test.push(c.ident);
+            }
+        }
+    }
+
+    let gen = quote! {
+    fn #varname(#inputs) -> Weight{
+        let _ = #body;
+        #ast_attr(#test)
     }
     };
     gen.into()
